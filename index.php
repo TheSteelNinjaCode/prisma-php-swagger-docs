50947518d3af82ae4db32712a286915be4f08004
TheSteelNinjaCode <thesteelninjacode@gmail.com>
2024-10-09 19:12:19 -0600

    Refactor index.php to use MainLayout class for adding CSS styles and scripts

--- index.php
+++ index.php
@@ -1,15 +1,24 @@
 <?php
 
+use Lib\MainLayout;
+
 $mainLayoutHead = [
     "<link rel='stylesheet' type='text/css' href='$baseUrl/swagger-docs/dist/swagger-ui.css' />",
     "<link rel='stylesheet' type='text/css' href='$baseUrl/swagger-docs/dist/index.css' />",
     "<link rel='icon' type='image/png' href='$baseUrl/swagger-docs/dist/favicon-32x32.png' sizes='32x32' />",
     "<link rel='icon' type='image/png' href='$baseUrl/swagger-docs/dist/favicon-16x16.png' sizes='16x16' />",
-]
+];
+
+MainLayout::addHeadScript(...$mainLayoutHead);
+
+$mainLayoutFooter = [
+    "<script src='<?= $baseUrl ?>/swagger-docs/dist/swagger-ui-bundle.js' charset='UTF-8'></script>",
+    "<script src='<?= $baseUrl ?>/swagger-docs/dist/swagger-ui-standalone-preset.js' charset='UTF-8'></script>",
+    "<script src='<?= $baseUrl ?>/swagger-docs/dist/swagger-initializer.js' charset='UTF-8'></script>",
+];
+
+MainLayout::addFooterScript(...$mainLayoutFooter);
 
 ?>
 
 <div id="swagger-ui"></div>
-<script src="<?= $baseUrl ?>/swagger-docs/dist/swagger-ui-bundle.js" charset="UTF-8"> </script>
-<script src="<?= $baseUrl ?>/swagger-docs/dist/swagger-ui-standalone-preset.js" charset="UTF-8"> </script>
-<script src="<?= $baseUrl ?>/swagger-docs/dist/swagger-initializer.js" charset="UTF-8"> </script>
